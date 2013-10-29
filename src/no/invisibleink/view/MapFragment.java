3f0e5ca04f32167032076441721b03492183fadd
Xennis <code@xennis.org>
2013-10-29 19:57:12 +0100

    .

--- src/no/invisibleink/view/MapFragment.java
+++ src/no/invisibleink/view/MapFragment.java
@@ -1,22 +1,124 @@
 package no.invisibleink.view;
 
+import java.util.LinkedList;
+import java.util.List;
+
 import no.invisibleink.R;
-import no.invisibleink.R.layout;
+import no.invisibleink.core.Ink;
+import no.invisibleink.core.InkWell;
+import no.invisibleink.core.LocationManager;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 
 public class MapFragment extends Fragment {
+
+	/**
+	 * The fragment argument representing the section number for this
+	 * fragment.
+	 */
+	public static final String ARG_SECTION_NUMBER = "section_map";	
+	
+    /**
+     * Note that this may be null if the Google Play services APK is not available.
+     */
+    private GoogleMap mMap;
+    private List<Ink> inks;
+    private LocationManager locationHelper;
+
+/*    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_main_map);
+        setUpMapIfNeeded();
+        inks = InkWell.getInstance().getInks();
+    }*/
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		View rootView = inflater.inflate(R.layout.fragment_main_dummy,
+				container, false);
+		TextView dummyTextView = (TextView) rootView
+				.findViewById(R.id.section_label);
+		dummyTextView.setText(Integer.toString(getArguments().getInt(
+				ARG_SECTION_NUMBER)));
+		return rootView;
+	}    
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        setUpMapIfNeeded();
+    }
+
+	
+	
 	public MapFragment() {}
 	
-	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-		View rootView = inflater.inflate(R.layout.fragment_main_map,
-				container, false);
+//	@Override
+//	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+//		View rootView = inflater.inflate(R.layout.fragment_main_map,
+//				container, false);
 		
-		return rootView;
-	}
+//		SupportMapFragment mMap = (SupportMapFragment)rootView.findViewById(R.id.main_map);
+		
+//		return rootView;
+//	}
+	
+    /**
+     * Sets up the map if it is possible to do so (i.e., the Google Play services APK is correctly
+     * installed) and the map has not already been instantiated.. This will ensure that we only ever
+     * call {@link #setUpMap()} once when {@link #mMap} is not null.
+     * <p>
+     * If it isn't installed {@link SupportMapFragment} (and
+     * {@link com.google.android.gms.maps.MapView MapView}) will show a prompt for the user to
+     * install/update the Google Play services APK on their device.
+     * <p>
+     * A user can return to this FragmentActivity after following the prompt and correctly
+     * installing/updating/enabling the Google Play services. Since the FragmentActivity may not have been
+     * completely destroyed during this process (it is likely that it would only be stopped or
+     * paused), {@link #onCreate(Bundle)} may not be called again so we should call this method in
+     * {@link #onResume()} to guarantee that it will be called.
+     */
+    private void setUpMapIfNeeded() {
+        // Do a null check to confirm that we have not already instantiated the map.
+        if (mMap == null) {
+            // Try to obtain the map from the SupportMapFragment.
+            mMap = ((SupportMapFragment) getFragmentManager().findFragmentById(R.id.main_map))
+                    .getMap();
+            // Check if we were successful in obtaining the map.
+            if (mMap != null) {
+                setUpMap();
+            }
+        }
+    }
+
+
+    /**
+     * This is where we can add markers or lines, add listeners or move the camera. In this case, we
+     * just add a marker near Africa.
+     * <p>
+     * This should only be called once and when we are sure that {@link #mMap} is not null.
+     */
+    private void setUpMap() {
+    	// TODO:
+//    	inks = new LinkedList<Ink>();
+    	
+//    	locationHelper = new LocationHelper(this.mMap, this.inks);
+    	
+    	mMap.setMyLocationEnabled(true);
+		mMap.setIndoorEnabled(true);
+    	mMap.setOnMyLocationChangeListener(locationHelper);
+    	mMap.setOnCameraChangeListener(locationHelper);
+    }    
 }
