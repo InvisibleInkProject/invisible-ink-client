88e5f20aa6c504c3234796d777b1b04f22131e18
Xennis <code@xennis.org>
2014-01-20 17:43:18 +0100

    Map works

--- ink_client/src/no/invisibleink/view/MapActivity.java
+++ ink_client/src/no/invisibleink/view/MapActivity.java
@@ -1,5 +1,8 @@
 package no.invisibleink.view;
 
+import java.util.Observable;
+import java.util.Observer;
+
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
@@ -7,10 +10,13 @@ import com.google.android.gms.maps.model.MarkerOptions;
 
 import no.invisibleink.R;
 import no.invisibleink.core.InkWell;
+import no.invisibleink.core.inks.Ink;
+import no.invisibleink.core.inks.InkList;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.util.Log;
 
-public class MapActivity extends FragmentActivity {
+public class MapActivity extends FragmentActivity implements Observer {
 
 	private InkWell inkWell;
 	
@@ -24,8 +30,8 @@ public class MapActivity extends FragmentActivity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.fragment_map);
 		setUpIfNeeded();
-        setUpMapIfNeeded();		
-	}
+        setUpMapIfNeeded();	       
+ 	}
 
     @Override
     protected void onResume() {
@@ -35,9 +41,10 @@ public class MapActivity extends FragmentActivity {
     }
 
     private void setUpIfNeeded() {
-    	if (inkWell == null) {
-//    		inkWell = InkWell.getInstance();
-    	}
+//    	if (inkWell == null) {
+   		inkWell = InkWell.getInstance();
+        inkWell.addObserver(this);
+ //   	}
     }
     
     /**
@@ -78,8 +85,20 @@ public class MapActivity extends FragmentActivity {
     	mMap.setMyLocationEnabled(true);
 		mMap.setIndoorEnabled(true);
 // TODO:
-//    	mMap.setOnMyLocationChangeListener(inkWell.getLocationManager());   	
-        mMap.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
-    }    
+    	mMap.setOnMyLocationChangeListener(inkWell.getLocationManager());   	
+    }
+
+	@Override
+	public void update(Observable observable, Object data) {
+Log.d(this.getClass().getName(), "update(..)");	
+mMap.clear();
+		InkList inkList = (InkList) data;
+		for(Ink i : inkList) {
+			mMap.addCircle(i.getCircleOptions());
+			mMap.addMarker(i.getMarkerOptions());
+		}
+		// TODO Auto-generated method stub
+		
+	}    
     
 }
