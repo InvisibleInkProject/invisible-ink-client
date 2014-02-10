6c783dbd74ea2f07e4e98d704a300ed36879b890
RHeil <rheil@stud.hs-bremen.de>
2014-02-10 18:05:46 +0100

    differentiate inks stuff

--- ink_client/src/no/invisibleink/view/MapSectionFragment.java
+++ ink_client/src/no/invisibleink/view/MapSectionFragment.java
@@ -3,15 +3,19 @@ package no.invisibleink.view;
 import no.invisibleink.R;
 import no.invisibleink.model.Ink;
 import no.invisibleink.model.InkList;
+import android.graphics.Point;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.GoogleMap.OnCameraChangeListener;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
 
 
 public class MapSectionFragment extends Fragment {
@@ -60,6 +64,33 @@ public class MapSectionFragment extends Fragment {
             if (mMap != null) {
             	mMap.setMyLocationEnabled(true);
         		mMap.setIndoorEnabled(true);
+        		
+        		
+        		// I'll leave this in here for the moment, in case we go back to the idea of linking the zoom level of the map to the list :) 
+//        		mMap.setOnCameraChangeListener(new OnCameraChangeListener(){
+//
+//					@Override
+//					public void onCameraChange(CameraPosition position) {
+//						Log.d("zoom", "zoom level =" + mMap.getCameraPosition().zoom + ": " + zoomLevel(mMap.getCameraPosition().zoom));						
+//					}
+//					
+        		//calculate the diameter of the visible part of the map in meters based on the zoom level and device width
+//					private double zoomLevel(float zoom){
+//						double equatorLength = 4007500; // in meters
+//						//double widthInPixels = 
+//						Point size = new Point();
+//						getActivity().getWindowManager().getDefaultDisplay().getSize(size);
+//						double widthInPixels = size.x;
+//						Log.d("zoom", ""+widthInPixels);
+//					   // double metersPerPixel = equatorLength / 256;
+//						double widthInMpP = equatorLength/(256*Math.pow(2, (zoom-2)));
+//					    double widthInMeters = widthInMpP*widthInPixels;
+//						return widthInMeters;
+//					}
+//        			
+//        			//zoom level 10 = etwas größer als r=2000km, 11 = etwas kleiner als r=2000km, 14 = etwas größer als 612, 15 etwas kleiner als 612
+//        			
+//        		});
             }
         }
     } 
