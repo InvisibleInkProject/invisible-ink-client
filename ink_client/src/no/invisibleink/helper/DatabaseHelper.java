c19ac700ee9d01bb6092bce0184631578e5ebcfe
Xennis <code@xennis.org>
2014-02-18 00:02:38 +0100

    Add DatabaseHelper to store inks

---
+++ ink_client/src/no/invisibleink/helper/DatabaseHelper.java
@@ -0,0 +1,135 @@
+package no.invisibleink.helper;
+
+import java.util.Date;
+import no.invisibleink.model.Ink;
+import no.invisibleink.model.InkList;
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.location.Location;
+import android.util.Log;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+	 
+    // Logcat tag
+    private static final String LOG = "DatabaseHelper";
+ 
+    // Database Version
+    private static final int DATABASE_VERSION = 1;
+ 
+    // Database Name
+    private static final String DATABASE_NAME = "inkCliet";
+ 
+    // Table Names
+    private static final String TABLE_INKS = "inks_message";
+ 
+    // Common column names
+    private static final String KEY_ID = "id";
+    private static final String KEY_CREATED_AT = "created_at";
+ 
+    // INKS Table - column names
+    private static final String KEY_USER_ID = "user_id";
+    private static final String KEY_TEXT = "text";
+    private static final String KEY_LOCATION_LAT = "location_lat";
+    private static final String KEY_LOCATION_LON = "location_lon";
+    private static final String KEY_RADIUS = "radius";
+ 
+    // Table Create Statements
+    // Inks table create statement
+    private static final String CREATE_TABLE_INKS = "CREATE TABLE "
+            + TABLE_INKS + "("
+    		+ KEY_ID + " INTEGER PRIMARY KEY,"
+    		+ KEY_TEXT + " TEXT,"
+            + KEY_RADIUS + " DOUBLE)";
+  
+    public DatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+ 
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        // creating required tables
+        db.execSQL(CREATE_TABLE_INKS);
+    }
+ 
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        // on upgrade drop older tables
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_INKS);
+ 
+        // create new tables
+        onCreate(db);
+    }
+    
+	/**
+	 * Inserts an Ink the database.
+	 * 
+	 * @param ink
+	 *            Ink
+	 * @return True, if insert was successful
+	 */
+    public boolean insertInk(Ink ink) {
+        SQLiteDatabase db = this.getWritableDatabase();
+     
+        ContentValues values = new ContentValues();
+        values.put(KEY_ID, ink.getID());
+        values.put(KEY_TEXT, ink.getMessage());
+        values.put(KEY_RADIUS, ink.getRadius());
+        values.put(KEY_LOCATION_LAT, ink.getLocation().getLatitude());
+        values.put(KEY_LOCATION_LON, ink.getLocation().getLongitude());
+        
+        // insert row, returns: the row ID of the newly inserted row, or -1 if an error occurred 
+        long row_id = db.insert(TABLE_INKS, null, values);
+        
+        return row_id > 0;
+    }
+    
+
+    /**
+     * Get all stored Inks.
+     * 
+     * @return All stored Inks
+     */
+    public InkList getInkList() {
+        InkList inkList = new InkList();
+        String selectQuery = "SELECT  * FROM " + TABLE_INKS;
+     
+        Log.e(LOG, selectQuery);
+     
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor c = db.rawQuery(selectQuery, null);
+     
+        // looping through all rows and adding to list
+        if (c.moveToFirst()) {
+            do {
+            	int id = c.getInt((c.getColumnIndex(KEY_ID)));
+            	Location location = null;
+            	double radius = c.getDouble(c.getColumnIndex(KEY_RADIUS));
+            	String title = "";
+            	String message = c.getString(c.getColumnIndex(KEY_TEXT));
+            	String author = "";
+            	Date date = null;
+            	Ink ink = new Ink(id, location, radius, title, message, author, date);     
+            	inkList.add(ink);
+            } while (c.moveToNext());
+        }
+     
+        return inkList;
+    }
+    
+    /*
+     * Deleting a todo
+     */
+    /**
+     * Delete specific Iink.
+     * 
+     * @param ink_id Delete an Ink
+     */
+    public void deleteInk(int ink_id) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        db.delete(TABLE_INKS, KEY_ID + " = ?",
+                new String[] { String.valueOf(ink_id) });
+    }    
+}
--- ink_client/src/no/invisibleink/view/MapSectionFragment.java
+++ ink_client/src/no/invisibleink/view/MapSectionFragment.java
@@ -3,19 +3,15 @@ package no.invisibleink.view;
 import no.invisibleink.R;
 import no.invisibleink.model.Ink;
 import no.invisibleink.model.InkList;
-import android.graphics.Point;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.GoogleMap.OnCameraChangeListener;
 import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.CameraPosition;
 
 
 public class MapSectionFragment extends Fragment {
