79f23f210a927a171744c321d63cf0a3905670b1
Xennis <code@xennis.org>
2014-07-06 02:36:22 +0200

    Candy

--- Ink_API/src/no/invisibleink/test_api/MainActivity.java
+++ Ink_API/src/no/invisibleink/test_api/MainActivity.java
@@ -13,6 +13,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
 
+@SuppressWarnings("unused")
 public class MainActivity extends Activity {
 	
 	public static final String TAG = "Test";
